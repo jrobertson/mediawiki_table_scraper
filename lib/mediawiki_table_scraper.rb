1ea566b4bea3a4d5a8b025638a788a296bac3d16
James Robertson <digital.robertson@gmail.com>
2022-06-05 15:29:57 +0100

    bug fix: Added the *tbody* reference into the *xpath* as used in the mediawiki HTML.

--- lib/mediawiki_table_scraper.rb
+++ lib/mediawiki_table_scraper.rb
@@ -10,40 +10,44 @@ class MediaWikiTableScraper
 
   attr_reader :tables
 
+
   def initialize(url)
 
     doc = Nokorexi.new(url).to_doc
 
     tables = doc.root.css('.wikitable')
+
     # Fetch the records as an array of hash records for each table
 
-    @tables = tables.map do |table|
+    @tables = tables.map.with_index do |table, i|
 
-      rows = table.xpath 'tr'
+      puts 'i: ' + i.inspect
+
+      rows = table.xpath 'tbody/tr'
 
       # fetch the column names
       labels = rows.shift.xpath 'th/text()'
-      names = labels.map {|x| x.downcase.to_sym }
+      names = labels.map {|x| x.chomp.downcase.to_sym }
 
-      a = rows.map do |row| 
+      a = rows.map do |row|
 
-        row.xpath('td').map do |x|
+        row.xpath('td').map do |col|
 
-          if x.has_elements? then
+          if col.has_elements? then
 
-            x.children.map do |c| 
+            col.children.map do |c|
               c.is_a?(String) ? c : c.xml.gsub(/<\/?\w+[^>]*>/,'')
             end.join ' '
 
           else
-            x.text.to_s
+            col.text.to_s
           end
 
         end
 
       end
 
-      a2 = a.map {|rows| names.zip(rows).to_h }
+      a.map {|rows| names.zip(rows).to_h }
 
     end
   end
@@ -53,4 +57,3 @@ class MediaWikiTableScraper
   end
 
 end
-
--- mediawiki_table_scraper.gemspec
+++ mediawiki_table_scraper.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = 'mediawiki_table_scraper'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Scrapes the tables from a MediaWiki page.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/mediawiki_table_scraper.rb']
-  s.add_runtime_dependency('nokorexi', '~> 0.3', '>=0.3.1')
+  s.add_runtime_dependency('nokorexi', '~> 0.7', '>=0.7.0')
   s.signing_key = '../privatekeys/mediawiki_table_scraper.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@r0bertson.co.uk'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/mediawiki_table_scraper'
 end
